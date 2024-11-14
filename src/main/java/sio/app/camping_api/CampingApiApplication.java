3c85b1c7708978fcfddd0e8a029dd0f41e218583
Tom_dt <114017367+TxMy-Elu@users.noreply.github.com>
2024-11-14 15:10:52 +0100

    add files

---
+++ src/main/java/sio/app/camping_api/CampingApiApplication.java
@@ -0,0 +1,13 @@
+package sio.app.camping_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sio/app/camping_api/CampingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sio.app.camping_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
